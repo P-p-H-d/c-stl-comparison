a872bd37983b210f7bca0f2745dcf93274392d3d
Patrick Pelissier <patrick.pelissier@gmail.com>
2023-03-27 00:03:39 +0200

    Port to using internal glib string

--- array-str/array-glib.c
+++ array-str/array-glib.c
@@ -6,18 +6,20 @@
 static gint
 str_cmp(gconstpointer a, gconstpointer b)
 {
-  const char *const*pa = a;
-  const char *const*pb = b; 
-  return strcmp(*pa, *pb);
+  const GString *const *pa = a;
+  const GString *const *pb = b;
+  return strcmp((*pa)->str, (*pb)->str);
 }
 
 int main(void)
 {
-  GArray *a = g_array_new(FALSE, FALSE, sizeof(char *));
+  GArray *a = g_array_new(FALSE, FALSE, sizeof(GString *));
   if (!a) {
     abort();
   }
-  char * i1 = "Hello", *i2 = "World", *i3 = "!";
+  GString *i1 = g_string_new("Hello");
+  GString *i2 = g_string_new("World");
+  GString *i3 = g_string_new("!");
   a = g_array_append_vals(a, &i1, 1);
   if (!a) {
     abort();
@@ -32,8 +34,11 @@ int main(void)
   }
   g_array_sort(a, str_cmp);
   for(int i = 0 ; i < a->len; i++) {
-    printf("%s\n", g_array_index(a, char *, i));
+    printf("%s\n", g_array_index(a, GString *, i)->str);
   }
   g_array_free(a, TRUE);
+  g_string_free(i1, TRUE);
+  g_string_free(i2, TRUE);
+  g_string_free(i3, TRUE);
   return 0;
 }
