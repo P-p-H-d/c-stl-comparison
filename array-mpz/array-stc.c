a0a834b64954551c3e77106993fbb7d6cb33c966
Patrick Pelissier <Patrick.Pelissier@gmail.com>
2023-05-12 22:57:43 +0200

    Proper encapsulation of argument in macro as it breaks with newest STC

--- array-mpz/array-stc.c
+++ array-mpz/array-stc.c
@@ -8,12 +8,12 @@ typedef struct {
 
 #define i_type         array_mpz
 #define i_val          mpz_z
-#define i_cmp(a,b)     (*a < *b ? -1 : *a > *b)                          // WORKAROUND: i_cmp only works on valraw (not on the base type): the sort cannot therefore handle the full mpz_t range.
-#define i_valdrop(z)   mpz_clear(z->Z)
-#define i_valclone(z)  ({ mpz_z Z; mpz_init_set(Z.Z, z.Z); Z; })  // WORKAROUND: needs to use GNU C extension or static inline functions to match GMP prototype.
+#define i_cmp(a,b)     (*(a) < *(b) ? -1 : *(a) > *(b))                          // WORKAROUND: i_cmp only works on valraw (not on the base type): the sort cannot therefore handle the full mpz_t range.
+#define i_valdrop(z)   mpz_clear((z)->Z)
+#define i_valclone(z)  ({ mpz_z Z; mpz_init_set(Z.Z, (z).Z); Z; })  // WORKAROUND: needs to use GNU C extension or static inline functions to match GMP prototype.
 #define i_valraw       unsigned int
-#define i_valfrom(ui)  ({ mpz_z Z; mpz_init_set_ui(Z.Z, ui); Z; })
-#define i_valto(z)     mpz_get_ui(z->Z)
+#define i_valfrom(ui)  ({ mpz_z Z; mpz_init_set_ui(Z.Z, (ui)); Z; })
+#define i_valto(z)     mpz_get_ui((z)->Z)
 #include <stc/cvec.h>
 
 int main(void)
