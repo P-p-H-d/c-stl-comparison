0aaf1545ac987724950358a4cc910cc976e13213
Patrick Pelissier <patrick.pelissier@gmail.com>
2025-04-12 11:07:36 +0200

    Port to new STC API

--- bench/bench-stc.c
+++ bench/bench-stc.c
@@ -8,76 +8,76 @@
 
 /********************************************************************************************/
 
-#define i_val unsigned int
+#define i_key unsigned int
 #define i_tag uint
-#include <stc/cvec.h>
+#include <stc/vec.h>
 
 static void test_array(size_t n)
 {
-  cvec_uint a1 = cvec_uint_init(), a2 = cvec_uint_init();
+  vec_uint a1 = vec_uint_init(), a2 = vec_uint_init();
   
   for(size_t i = 0; i < n; i++) {
-    cvec_uint_push_back(&a1, rand_get() );
-    cvec_uint_push_back(&a2, rand_get() );
+    vec_uint_push_back(&a1, rand_get() );
+    vec_uint_push_back(&a2, rand_get() );
   }
   unsigned int s = 0;
   for(unsigned long i = 0; i < n; i++) {
-    s += *cvec_uint_at(&a1, i ) * *cvec_uint_at(&a2, i );
+    s += *vec_uint_at(&a1, i ) * *vec_uint_at(&a2, i );
   }
   g_result = s;
 
-  cvec_uint_drop(&a1);
-  cvec_uint_drop(&a2);
+  vec_uint_drop(&a1);
+  vec_uint_drop(&a2);
 }
 
 /********************************************************************************************/
-#define i_val unsigned int
+#define i_key unsigned int
 #define i_tag uint
-#include <stc/clist.h>
+#include <stc/list.h>
 
 static void test_list(size_t n)
 {
-  clist_uint a1 = clist_uint_init(), a2 = clist_uint_init();
+  list_uint a1 = list_uint_init(), a2 = list_uint_init();
   
   for(size_t i = 0; i < n; i++) {
-    clist_uint_push_front(&a1, rand_get() );
-    clist_uint_push_front(&a2, rand_get() );
+    list_uint_push_front(&a1, rand_get() );
+    list_uint_push_front(&a2, rand_get() );
   }
   unsigned int s = 0;
-  for(clist_uint_iter it1 = clist_uint_begin(&a1), it2 = clist_uint_begin(&a2) ;
-      it1.ref != clist_uint_end(&a1).ref ;
-      clist_uint_next(&it1), clist_uint_next(&it2) ) {
+  for(list_uint_iter it1 = list_uint_begin(&a1), it2 = list_uint_begin(&a2) ;
+      it1.ref != list_uint_end(&a1).ref ;
+      list_uint_next(&it1), list_uint_next(&it2) ) {
     s += *it1.ref * *it2.ref;
   }
   g_result = s;
 
-  clist_uint_drop(&a1);
-  clist_uint_drop(&a2);
+  list_uint_drop(&a1);
+  list_uint_drop(&a2);
 }
 
 /********************************************************************************************/
 
 #define i_tag ulong
 #define i_key unsigned long
-#include <stc/csset.h>
+#include <stc/sset.h>
 
 static void test_rbtree(size_t n)
 {
-  csset_ulong tree = csset_ulong_init();
+  sset_ulong tree = sset_ulong_init();
   
   for (size_t i = 0; i < n; i++) {
-    csset_ulong_insert(&tree, rand_get() );
+    sset_ulong_insert(&tree, rand_get() );
   }
     
   rand_init();
   unsigned int s = 0;
   for (size_t i = 0; i < n; i++) {
     unsigned long j = rand_get();
-    if (csset_ulong_get(&tree, j ) != NULL)
+    if (sset_ulong_get(&tree, j ) != NULL)
       s+= j;
   }
   g_result = s;
-  csset_ulong_drop(&tree);
+  sset_ulong_drop(&tree);
 }
 
 /********************************************************************************************/
@@ -85,28 +85,28 @@ static void test_rbtree(size_t n)
 #define i_tag ulong
 #define i_key unsigned long
 #define i_val unsigned long
-#include <stc/cmap.h>
+#include <stc/hmap.h>
 
 static void
 test_dict(size_t  n)
 {
-  cmap_ulong dict = cmap_ulong_init();
+  hmap_ulong dict = hmap_ulong_init();
   
   for (size_t i = 0; i < n; i++) {
     unsigned long value = rand_get();
     unsigned long key = rand_get();
-    cmap_ulong_insert_or_assign(&dict, key, value);
+    hmap_ulong_insert_or_assign(&dict, key, value);
   }
   rand_init();
   unsigned int s = 0;
   for (size_t i = 0; i < n; i++) {
-    const cmap_ulong_value*val = cmap_ulong_get(&dict, rand_get());
+    const hmap_ulong_value*val = hmap_ulong_get(&dict, rand_get());
     if (val != NULL)
       s += val->second;
   }
   g_result = s;
   
-  cmap_ulong_drop(&dict);
+  hmap_ulong_drop(&dict);
 }
 
 /********************************************************************************************/
@@ -130,50 +130,50 @@ static inline size_t char_hash(const char_array_t *a) {
 #define i_val       char_array_t
 #define i_hash      char_hash
 #define i_eq        char_equal_p
-#include <stc/cmap.h>
+#include <stc/hmap.h>
 
 static void
 test_dict_big(size_t  n)
 {
-  cmap_achar dict = cmap_achar_init();
+  hmap_achar dict = hmap_achar_init();
 
   for (size_t i = 0; i < n; i++) {
     char_array_t s1, s2;
     sprintf(s1.a, "%u", rand_get());
     sprintf(s2.a, "%u", rand_get());
-    cmap_achar_insert_or_assign(&dict, s1, s2);
+    hmap_achar_insert_or_assign(&dict, s1, s2);
   }
   rand_init();
   unsigned int s = 0;
   for (size_t i = 0; i < n; i++) {
     char_array_t s1;
     sprintf(s1.a, "%u", rand_get());
-    const cmap_achar_value*val = cmap_achar_get(&dict, s1);
+    const hmap_achar_value*val = hmap_achar_get(&dict, s1);
     if (val != NULL)
       s ++;
   }
   g_result = s;
 
-  cmap_achar_drop(&dict);
+  hmap_achar_drop(&dict);
 }
 
 /********************************************************************************************/
 
-#define i_val float
+#define i_key float
 #define i_tag float
 #define i_use_cmp
-#include <stc/cvec.h>
+#include <stc/vec.h>
 
 static void test_sort(size_t n)
 {
-  cvec_float a1 = cvec_float_init();
+  vec_float a1 = vec_float_init();
 
   for(size_t i = 0; i < n; i++) {
-    cvec_float_push_back(&a1, rand_get() );
+    vec_float_push_back(&a1, rand_get() );
   }
-  cvec_float_sort(&a1);
-  g_result = *cvec_float_at(&a1, 0);
-  cvec_float_drop(&a1);
+  vec_float_sort(&a1);
+  g_result = *vec_float_at(&a1, 0);
+  vec_float_drop(&a1);
 }
 
 /********************************************************************************************/
